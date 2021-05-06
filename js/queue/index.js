232146e2b9144facb21b2db6f596952aed6aa9db
Brian Sai <brianwsai@gmail.com>
2021-05-06 11:07:52 -0700

    Created and tested queue class

---
+++ js/queue/index.js
@@ -0,0 +1,33 @@
+// --- Description
+// Create a queue data structure.  The queue
+// should be a class with methods 'add' and 'remove'.
+// Adding to the queue should store an element until
+// it is removed
+// --- Examples
+//     const q = new Queue();
+//     q.add(1);
+//     q.remove(); // returns 1;
+
+class Queue {
+  constructor() {
+    this.data = [];
+  }
+
+  add(record) {
+    this.data.unshift(record);
+  }
+
+  remove() {
+    return this.data.pop();
+  }
+
+  peek(record) {
+    return record[record.length - 1];
+  }
+
+  isEmpty() {
+    return !this.data.length;
+  }
+}
+
+module.exports = Queue;
---
+++ js/queue/test.js
@@ -0,0 +1,41 @@
+const Queue = require('./index');
+
+test('Queue is a class', () => {
+  expect(typeof Queue.prototype.constructor).toEqual('function');
+});
+
+test('can add elements to a queue', () => {
+  const q = new Queue();
+  expect(() => {
+    q.add(1);
+  }).not.toThrow();
+});
+
+test('can remove elements from a queue', () => {
+  const q = new Queue();
+  expect(() => {
+    q.add(1);
+    q.remove();
+  }).not.toThrow();
+});
+
+test('Order of elements is maintained', () => {
+  const q = new Queue();
+  q.add(1);
+  q.add(2);
+  q.add(3);
+  expect(q.remove()).toEqual(1);
+  expect(q.remove()).toEqual(2);
+  expect(q.remove()).toEqual(3);
+  expect(q.remove()).toEqual(undefined);
+});
+
+test('Order of elements is maintained', () => {
+  const q = new Queue();
+  q.add(1);
+  console.log(q);
+  expect(q.isEmpty()).toEqual(false);
+
+  q.remove();
+  expect(q.isEmpty()).toEqual(true);
+});
