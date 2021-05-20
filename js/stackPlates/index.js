3dfb541d6d69090fdf7eb40ce6a32c1a2c3527ca
Brian Sai <brianwsai@gmail.com>
2021-05-20 13:17:58 -0700

    Created stack plates pop at method, allowed for push at the same given index until capacity, and tested

--- js/stackPlates/index.js
+++ js/stackPlates/index.js
@@ -2,12 +2,16 @@ class StackPlates {
   constructor(capacity) {
     this.stacks = [];
     this.capacity = capacity;
+    this.currentIndex = null;
   }
 
   push(num) {
     const lastStack = this.stacks[this.stacks.length - 1];
     if (!lastStack || lastStack.length === this.capacity) {
       this.stacks.push([num]);
+    } else if (this.currentIndex) {
+      this.stacks[this.currentIndex].push(num);
+      this.currentIndex = null;
     } else {
       lastStack.push(num);
     }
@@ -15,12 +19,21 @@ class StackPlates {
   }
 
   pop() {
-    const lastStack = this.stacks[this.stacks.length - 1];
+    const lastStack = this.stacks[this.currentIndex || this.stacks.length - 1];
     const popped = lastStack.pop();
-    if (lastStack < 1) {
+    if (lastStack.length < 1) {
       this.stacks.pop();
     }
     return popped;
+  }
+
+  popAt(index) {
+    if (index < this.stacks.length) {
+      this.currentIndex = index;
+      return this.pop();
+    } else {
+      throw new Error('Given index exceeds stack length.');
+    }
   }
 }
 
--- js/stackPlates/test.js
+++ js/stackPlates/test.js
@@ -37,3 +37,28 @@ test('stack can remove items', () => {
 
   expect(s.stacks).toEqual([]);
 });
+
+test('stack can remove items at given index and push back into that same stack', () => {
+  const s = new StackPlates(2);
+  const result1 = [[3, 4], [89], [45]];
+  s.push(3);
+  s.push(4);
+  s.push(89);
+  s.push(48);
+  s.push(45);
+  s.popAt(1);
+  expect(s.stacks).toEqual(result1);
+
+  const result2 = [[3, 4], [89, 39], [45]];
+  s.push(39);
+  expect(s.stacks).toEqual(result2);
+
+  const result3 = [
+    [3, 4],
+    [89, 39],
+    [45, 99],
+  ];
+
+  s.push(99);
+  expect(s.stacks).toEqual(result3);
+});
