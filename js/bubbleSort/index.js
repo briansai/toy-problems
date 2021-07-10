f786d953b77d7d69f31859252e2ebe5e8c8d49d7
Brian Sai <brianwsai@gmail.com>
2021-07-09 18:00:24 -0700

    Created bubble sort file

---
+++ js/bubbleSort/index.js
@@ -0,0 +1,30 @@
+const swap = (i, j, array) => {
+  var temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+  return array;
+};
+
+const bubbleSort = (numbers) => {
+  // // create variable sorted as false
+  // // while the variable is false
+  // // change variable to true
+  // // iterate through numbers
+  // // if current number is greater than the nxt number,
+  // // swap the current and next number
+  // // return numbers
+  let sorted = false;
+  while (!sorted) {
+    sorted = true;
+    for (let x = 0; x < numbers.length; x++) {
+      if (numbers[x] > numbers[x + 1]) {
+        sorted = false;
+        swap(x, x + 1, numbers);
+      }
+    }
+  }
+  return numbers;
+};
+
+bubbleSort([1, 4, -2, 30, 64, 20, 35, 87, 65]);
+module.exports = bubbleSort;
